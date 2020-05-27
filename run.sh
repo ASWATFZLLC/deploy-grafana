e8dab2feff56bc72491edc3cce273c2c9964d059
Loic Tosser <loic@kalvad.com>
2020-05-27 12:00:11 +0400

    build + run update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export GF_PATHS_PLUGINS=grafana-$GRAFANA_VERSION/data/plugins
-grafana-$GRAFANA_VERSION/bin/grafana-server
+grafana-$GRAFANA_VERSION/bin/grafana-server -homepath grafana-$GRAFANA_VERSION
